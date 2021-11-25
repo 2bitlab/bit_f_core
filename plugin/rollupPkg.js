d972841075cd6093c12d5b17d33b1405e407565e
thomas-ballo <liurongliang@balloai.com>
2021-11-26 00:25:03 +0800

    fix: change rollupPakg

--- plugin/rollupPkg.js
+++ plugin/rollupPkg.js
@@ -1,8 +1,10 @@
 // 为了将引入的 npm 包，也打包进最终结果中
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
+
+import buble from 'rollup-plugin-buble'
 import dts from 'rollup-plugin-dts'
-import { terser } from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser' // 生成最小化的包
 
 const extensions = ['.ts']
 
@@ -12,7 +14,13 @@ export default ({
   input = './src/index.ts',
   context = 'window'
 }) => {
-  const { version, types } = pkg || {}
+  const { version, types, author } = pkg || {}
+
+  const banner = `/*!
+  * ${name} v${version}
+  * (c) ${new Date().getFullYear()} ${author}
+  * @license MIT
+  */`
 
   const baseOutput = {
     name,
@@ -24,7 +32,8 @@ export default ({
         .replace(/@/g, '_')
         .replace(/-/g, '_')
         .replace(/\//g, '_')}_version = '${version}'
-    }`
+    }
+    ${banner}`
   }
 
   return [
@@ -46,13 +55,33 @@ export default ({
         },
         {
           ...baseOutput,
-          file: 'lib/index.cjs.js', // package.json 中 "main": "lib/index.cjs.js",
+          plugins: [buble()],
+          sourcemap: false,
+          file: 'lib/index.esm.browser.js',
+          format: 'esm' // umd 兼容形式的包， 可以直接应用于网页 script
+        },
+        {
+          ...baseOutput,
+
+          plugins: [buble(), terser()],
+          file: 'lib/index.esm.browser.min.js',
+          format: 'esm' // umd 兼容形式的包， 可以直接应用于网页 script
+        },
+        {
+          ...baseOutput,
+          file: 'lib/index.common.js', // package.json 中 "main": "lib/index.cjs.js",
           format: 'cjs' // commonjs 形式的包， require 导入
         },
         {
           ...baseOutput,
-          file: 'lib/index.umd.js',
-
+          file: 'lib/index.js',
+          plugins: [],
+          sourcemap: false,
+          format: 'umd' // umd 兼容形式的包， 可以直接应用于网页 script
+        },
+        {
+          ...baseOutput,
+          file: 'lib/index.min.js',
           format: 'umd' // umd 兼容形式的包， 可以直接应用于网页 script
         }
       ],
