ddb72bfa1974165b7a49368cfee132a060642e6e
thomas-ballo <liurongliang@balloai.com>
2021-11-29 13:50:07 +0800

    fix: initConfigMixin

--- packages/state/src/configModule/utils.ts
+++ packages/state/src/configModule/utils.ts
@@ -1,55 +1,80 @@
-import { merge } from 'lodash'
+import { merge, isEqual, cloneDeep } from 'lodash'
 
 /**
  * 提供 config 的mixin
  */
-export const initConfigMixin = ({
-  mapState,
-  configModuleName = 'configModule'
-}) => {
+export const initConfigMixin = ({ configModuleName = 'configModule' }) => {
   return {
-    data() {
-      return {
-        compConfig: {}
+    props: {
+      //父组件的重写
+      overwriteConfig: {
+        type: Object,
+        default: () => ({})
       }
     },
-    create() {
-      // 处理父组件的重写
-      const { config, _componentTag } = this.$options || {}
-      const { path } = this.$route || {}
-      console.log(path, '_componentTag = ', _componentTag, 'config =', config)
-
-      this.compConfig = config
-      // TODO 父组件的重写
-
-      // TODO i18n
-      // TODO ui
-
-      // TODO API
-      // TODO showMap
-      // TODO variable
-      // TODO 数据功能的只显示那些，不显示哪些
+    data() {
+      const { config } = this.$options || {}
+      // const { path } = this.$route || {}
+      // console.log(path, '_componentTag = ', _componentTag, 'config =', config)
+      return {
+        compConfig: config || {}
+      }
     },
     computed: {
-      ...mapState(configModuleName, ['sysConfigMap', 'userConfigMap']),
-      currentConfig({ sysConfigMap, userConfigMap, compConfig }) {
+      sysConfigMap() {
+        const state = (this.$store?.state || {})[configModuleName]
+        const { sysConfigMap } = state || {}
+        return sysConfigMap
+      },
+      userConfigMap() {
+        const state = (this.$store?.state || {})[configModuleName]
+        const { userConfigMap } = state || {}
+        return userConfigMap
+      },
+      pathConfig({ sysConfigMap, userConfigMap }) {
         const { path } = this.$route || {}
-        const sysConfig = sysConfigMap[path]
-        const userConfig = userConfigMap[path]
-
-        let cacheConfig = compConfig
-        cacheConfig = merge(sysConfig, cacheConfig)
-        cacheConfig = merge(userConfig, cacheConfig)
-
-        console.log(
-          'currentConfig change',
-          path,
-          sysConfig,
-          userConfig,
-          cacheConfig
+        let sysConfig
+        let userConfig
+        if (path) {
+          sysConfig = (sysConfigMap || {})[path]
+          userConfig = (userConfigMap || {})[path]
+        }
+        return merge(cloneDeep(sysConfig || {}), cloneDeep(userConfig || {}))
+      },
+      currentConfig({ compConfig, overwriteConfig, pathConfig }) {
+        let cacheConfig = compConfig || {}
+        cacheConfig = merge(
+          cloneDeep(cacheConfig),
+          cloneDeep(overwriteConfig || {})
         )
+        cacheConfig = merge(cloneDeep(cacheConfig), cloneDeep(pathConfig || {}))
         return cacheConfig
+      },
+      variable({ currentConfig }) {
+        const { variable } = currentConfig || {}
+        return variable || {}
+      },
+      configI18n({ currentConfig }) {
+        const { i18n } = currentConfig || {}
+        return i18n
       }
-    } as any
+    } as any,
+    watch: {
+      configI18n: {
+        handler(newValue) {
+          const { locale, messages } = this.$i18n || {}
+          if (newValue && locale) {
+            const langObj = messages[locale] || {}
+            const newLangObj = newValue[locale] || {}
+            const afterMerge = merge(cloneDeep(langObj), cloneDeep(newLangObj))
+            const needSetLocaleMessage = !isEqual(afterMerge, langObj)
+            if (needSetLocaleMessage) {
+              this.$i18n.setLocaleMessage(locale, afterMerge)
+            }
+          }
+        },
+        immediate: true
+      } as any
+    }
   } as any
 }
